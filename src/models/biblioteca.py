198b43646bfb2515460b39b295f29267368731ca
wallace0003 <wallaceizidoro.1@gmail.com>
2025-10-11 23:55:44 -0300

    add some class

---
+++ src/models/biblioteca.py
@@ -0,0 +1,8 @@
+
+class Biblioteca:
+    def __init__(self, codigo_biblioteca:int, nome:str, endereco:str, email:str):
+        self.codigo_biblioteca = codigo_biblioteca
+        self.nome = nome
+        self.endereco = endereco
+        self.email = email
+    
---
+++ src/models/categoria.py
@@ -0,0 +1,6 @@
+
+class Categoria:
+    def __init__(self, codigo_livro:int, nome_categoria:str, codigo_biblioteca:str):
+        self.codigo_livro = codigo_livro
+        self.nome_categoria = nome_categoria
+        self.codigo_biblioteca = codigo_biblioteca
