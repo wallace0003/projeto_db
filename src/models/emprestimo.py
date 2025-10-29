059f466bdb67681c70dea915782e9443cf442185
Wallace dos Santos Izidoro <161092400+wallace0003@users.noreply.github.com>
2025-10-29 22:48:25 +0000

    fix: classe emprestimo

--- src/models/emprestimo.py
+++ src/models/emprestimo.py
@@ -1,11 +1,11 @@
 
 class Emprestimo:
-    def __init__(self, codigo_emprestimo: int, codigo_pessoa: int, codigo_livro: int,
-                 data_emprestimo: str, data_devolucao: str, status: str):
-        
-        self.codigo_emprestimo = codigo_emprestimo
-        self.codigo_pessoa = codigo_pessoa
-        self.codigo_livro = codigo_livro
+    def __init__(self, data_emprestimo:str, data_prevista_devolucao:str, data_devolucao:str,
+                funcionario_id: int, status:str, usuario_id:int, livro_id:int):
         self.data_emprestimo = data_emprestimo
+        self.data_prevista_devolucao = data_prevista_devolucao
         self.data_devolucao = data_devolucao
+        self.funcionario_id = funcionario_id
         self.status = status
+        self.usuario_id = usuario_id
+        self.livro_id = livro_id
