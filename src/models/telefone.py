b6a0fccd59711d81d65649cd45563e097f2859fa
Wallace dos Santos Izidoro <161092400+wallace0003@users.noreply.github.com>
2025-10-29 22:56:03 +0000

    fix: classe telefones_pessoa

--- src/models/telefone.py
+++
@@ -1,7 +0,0 @@
-
-class Telefone:
-    def __int__(self, codigo_pessoa:int, numero_telefone:str,
-                tipo:str):
-        self.codigo_pessoa = codigo_pessoa
-        self.numero_telefone = numero_telefone
-        self.tipo = tipo
